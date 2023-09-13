d6b8f68f00da533436d9cdaab31b2f55a8238bb4
rayin19 <shrey163@gmail.com>
2023-09-13 14:59:10 +0530

    Initial Commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/todo-app-vite-demo/",
+  base: "/todo-app-vite-demo/frontend",
   plugins: [react()],
 })
